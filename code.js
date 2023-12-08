e65fe8f0134a89a0437581e8d0eeb2060e2dd36a
boboestes14 <143558849+boboestes14@users.noreply.github.com>
2023-12-08 02:33:18 +0000

    commit

--- code.js
+++ code.js
@@ -1,16 +1,20 @@
-function depthFirstSearch(graph, startNode, targetNode) {
-    graph[startNode].push("v")
+function depthFirstSearch(graph, startNode, targetNode, trueStartNode) {
+    let cha = graph
+    cha[startNode].push("v")
     if(startNode == targetNode){
         return [targetNode];
     }
     
-    for(let n = 0; n < graph[startNode].length-1; n++){
-        if(graph[graph[startNode][n]][graph[graph[startNode][n]].length-1] != "v"){
-            a = [startNode].concat(depthFirstSearch(graph, graph[startNode][n], targetNode))
+    for(let n = 0; n < cha[startNode].length-1; n++){
+        if(cha[cha[startNode][n]][cha[cha[startNode][n]].length-1] != "v"){
+            a = [startNode].concat(depthFirstSearch(cha, cha[startNode][n], targetNode, trueStartNode))
             if(a[a.length-1] != -1){
                 return a}
             a.pop()
         }
+    }
+    if(startNode == trueStartNode){
+        return []
     }
     return [-1];
 }
--- code.test.js
+++ code.test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
 
-var a = [
+var graph = [
     [6,1,0,5],
     [2,7,6,3],
     [4,3,2,7],
@@ -13,10 +13,9 @@ var a = [
     [0,7,8,4],
     [6,3,1,4],
     [3,6,4,1]];
-a = JSON.stringify(a) 
-aAnswers = ["[0]", "[0,6,7,3,8,4,1]", "[0,6,7,3,8,4,1,2]", "[0,6,7,3]", 
+graphAnswers = ["[0]", "[0,6,7,3,8,4,1]", "[0,6,7,3,8,4,1,2]", "[0,6,7,3]", 
 "[0,6,7,3,8,4]", "[0,5]", "[0,6]", "[0,6,7]", "[0,6,7,3,8]"]
 
-for(jk = 0; jk < aAnswers.length-1; jk++){
-    assert(JSON.stringify(depthFirstSearch(JSON.parse(a), 0, jk)) == aAnswers[jk]);
+for(jk = 0; jk < graphAnswers.length-1; jk++){
+    assert(JSON.stringify(depthFirstSearch(JSON.parse(JSON.stringify(graph)), 0, jk, 0)) == graphAnswers[jk]);
 }
